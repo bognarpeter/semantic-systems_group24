c8ec17cc6eb4f07569772f6833a2f1835572d6fb
Peter Bognár <bognarpaeter@gmail.com>
2020-02-13 22:48:14 +0100

    querying db done

--- Source/web-app/routes/index.js
+++ Source/web-app/routes/index.js
@@ -1,30 +1,56 @@
 'use strict';
 
+var fetch = require('isomorphic-fetch')
+const {URL} = require('whatwg-url');
+var SparqlHttp = require('sparql-http-client')
 var router = require('express').Router();
+var Q = require('q');
 
-var query = function(query, res){
-    var options = {
-        method: 'GET',
-        uri: 'https://localhost:XXXX',
-        headers: {
-            "Content-Type": ''
-        },
-        query: {
-        },
-    };
 
-    rp(options)
-        .then(function (apiResponse) {
-              console.log(apiResponse);
-              res.render('/', {res: apiResponse, resClean: apiResponse});
-            })
-            .catch((err) => console.log(err));
+var getDiseases = function(symptoms, res){
+  var deferred = Q.defer();
+
+  SparqlHttp.fetch = fetch
+  SparqlHttp.URL = URL
+
+  var endpoint = new SparqlHttp({endpointUrl: 'https://localhost:7200'})
+  var query =
+   ['PREFIX : <http://www.semanticweb.org/christine/ontologies/2020/0/mdb#>',
+    'PREFIX foaf: <http://xmlns.com/foaf/0.1/>',
+    'SELECT ?name WHERE {?disease foaf:name ?name; :resulting ?symptom. FILTER(']
+
+  var contains = []
+  symptoms.map((symptom) => {
+    contains.push(`CONTAINS(?symptom, "${symptom}")`)
+  });
+  contains = contains.join('&&');
+  query.push(contains);
+  query.push(')}');
+  query = query.join('\n');
+
+  endpoint.selectQuery(query).then(function (res) {
+
+    deferred.resolve(res);
+    return deferred.promise;
+
+  }).catch(function (err) {
+    console.error(err)
+  })
 };
 
 
 // Get Homepage
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.post('/query', (req, res) => {
+    var query = req.body.query;
+    var symptoms = query.split(',');
+    var symptoms_cleaned = symptoms.map((s) => {return s.trim();});
+    getDiseases(symptoms_cleaned)
+      .then((diseases) => {res.render('results', {results: diseases});})
+      .catch((err) => {console.log(err)});
 });
 
 module.exports = router;
